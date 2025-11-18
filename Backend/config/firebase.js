63c86fc31ee13c15a1ade800824a92ac3f5c1e27
Teboho02 <86244195+Teboho02@users.noreply.github.com>
2025-11-18 10:13:05 +0200

    Use environment variables for config
    Replaced hardcoded Firebase configuration values with environment variables for better security.

--- Backend/config/firebase.js
+++ Backend/config/firebase.js
@@ -1,14 +1,17 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import dotenv from 'dotenv';
+dotenv.config();
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBRq8lvXWDQLpdeXL3oz59TBx38LvUPGRk",
-  authDomain: "mshashazwinepe-8489d.firebaseapp.com",
+  apiKey: process.env.APIKEY,
+  authDomain: process.env.AUTHDOMAUN,
   projectId: "mshashazwinepe-8489d",
-  storageBucket: "mshashazwinepe-8489d.firebasestorage.app",
+  storageBucket: process.env.STORAGEBUCKET,
   messagingSenderId: "1020679564289",
   appId: "1:1020679564289:web:93a9c669415deeaf42a400"
 };
